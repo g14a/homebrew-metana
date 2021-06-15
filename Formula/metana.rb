6005d66b37731dc46735ec6a6fb724a7a0c4f3e5
goreleaserbot <goreleaser@carlosbecker.com>
2021-06-15 11:50:35 +0530

    Brew formula update for metana version v2.2

--- Formula/metana.rb
+++ Formula/metana.rb
@@ -5,28 +5,28 @@
 class Metana < Formula
   desc "An abstract migration tool written in Go for Go services."
   homepage "https://github.com/g14a/metana"
-  version "2.1"
+  version "2.2"
   bottle :unneeded
 
   on_macos do
     if Hardware::CPU.intel?
-      url "https://github.com/g14a/metana/releases/download/v2.1/metana_2.1_darwin_amd64.tar.gz"
-      sha256 "9b12e95f1bea58a01b093b5d768106eaa72eae941cd8eb607d9ee6cc8aeb0686"
+      url "https://github.com/g14a/metana/releases/download/v2.2/metana_2.2_darwin_amd64.tar.gz"
+      sha256 "439ca801100baf84f135e34b5c483cf4fcd0611c6d422979e30303d946994728"
     end
     if Hardware::CPU.arm?
-      url "https://github.com/g14a/metana/releases/download/v2.1/metana_2.1_darwin_arm64.tar.gz"
-      sha256 "2008e2f9ee034793a70b25262c1383bdc8afead367ea4bba69028cc78a96ec3b"
+      url "https://github.com/g14a/metana/releases/download/v2.2/metana_2.2_darwin_arm64.tar.gz"
+      sha256 "2cfbf151c1468e26c0759306824420879df25393b1d03ab2c5afcdfe8e907711"
     end
   end
 
   on_linux do
     if Hardware::CPU.intel?
-      url "https://github.com/g14a/metana/releases/download/v2.1/metana_2.1_linux_amd64.tar.gz"
-      sha256 "f613f07183d70c3d510583fdf986c4e5ff3080d267169d2dd2b78955cb10431f"
+      url "https://github.com/g14a/metana/releases/download/v2.2/metana_2.2_linux_amd64.tar.gz"
+      sha256 "e675e16ec611b608a7be9caf404b020abb4d2364545ec22a05b3601d983b3f45"
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/g14a/metana/releases/download/v2.1/metana_2.1_linux_arm64.tar.gz"
-      sha256 "7d40a14bdc52245179f9c4be7210f81d2c70dbcbac946442fc73075d2438979d"
+      url "https://github.com/g14a/metana/releases/download/v2.2/metana_2.2_linux_arm64.tar.gz"
+      sha256 "69a1b15eef1f0ad5ba311d455f3b393d214836effcdb8d1030b107ec250ae016"
     end
   end
 
